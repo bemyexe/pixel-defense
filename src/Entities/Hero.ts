c40a73a084cf35b30873404d9bc01c04bd8c4a28
maxim <trifonov.max@mail.ru>
2025-01-26 23:17:07 +0300

    refactor(hero): added move methods

--- src/Entities/Hero.ts
+++ src/Entities/Hero.ts
@@ -1,8 +1,18 @@
 import {Graphics} from 'pixi.js';
 import {Entity} from './Entity';
 
+const PIXEL_SIZE = 16;
+
 export class Hero extends Entity {
   constructor(view: Graphics) {
     super(view);
+  }
+
+  public moveRigth() {
+    this.x += PIXEL_SIZE;
+  }
+
+  public moveLeft() {
+    this.x -= PIXEL_SIZE;
   }
 }
