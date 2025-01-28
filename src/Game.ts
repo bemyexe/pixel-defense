3e32b97ee6ccb32173f701fa2602eccc8ba22fc9
maxim <trifonov.max@mail.ru>
2025-01-28 14:52:24 +0300

    feature(game): added pause/start methods

--- src/Game.ts
+++ src/Game.ts
@@ -21,6 +21,9 @@ export default class Game {
   private delayCounter = 0;
   private gameSpeed = 0;
   private scoreCounter = 0;
+  private pauseBtn: Graphics;
+  // private restartBtn: Graphics;
+  private isGamePaused = false;
   private keyBoardHandlerBound: (event: KeyboardEvent) => void;
   constructor(app: Application) {
     this.app = app;
@@ -29,7 +32,10 @@ export default class Game {
       SCREEN_WIDTH / 2 - PIXEL_SIZE,
       SCREEN_HEIGHT - PIXEL_SIZE
     );
-
+    this.pauseBtn = this.createButtons(240, 0, 'Pause', () => this.pauseGame());
+    // this.restartBtn = this.createButtons(300, 0, 'Retry', () =>
+    //   console.log('Retry')
+    // );
     this.hero = new Hero(heroView);
 
     this.keyBoardHandlerBound = this.keyBoardHandler.bind(this);
@@ -38,7 +44,7 @@ export default class Game {
 
   public update() {
     if (this.isFail()) {
-      this.stopGame();
+      this.gameOver();
       return;
     }
 
@@ -48,7 +54,6 @@ export default class Game {
         enemy.update();
       });
     }
-
     this.moveBullets();
     this.clear();
   }
@@ -58,6 +63,7 @@ export default class Game {
   }
 
   private keyBoardHandler(event: KeyboardEvent) {
+    if (this.isGamePaused && event.key !== 'Enter') return;
     switch (event.key) {
       case 'ArrowRight': {
         if (this.hero.x < SCREEN_WIDTH - PIXEL_SIZE) {
@@ -75,6 +81,10 @@ export default class Game {
         const bulletView = this.createPixel(this.hero.x, this.hero.y);
         const bullet = new Bullet(bulletView);
         this.bullets.push(bullet);
+        break;
+      }
+      case 'Enter': {
+        this.pauseGame();
         break;
       }
     }
@@ -139,9 +149,9 @@ export default class Game {
     return this.enemies.find((enemy) => enemy.y === this.hero.y) !== undefined;
   }
 
-  private stopGame() {
+  private gameOver() {
     document.removeEventListener('keydown', this.keyBoardHandlerBound);
-    this.app.ticker.remove(this.update, this);
+
     this.scoreText.text = `You lose! Score: ${this.scoreCounter}`;
   }
 
@@ -155,6 +165,45 @@ export default class Game {
     view.x = x;
     view.y = y;
     this.app.stage.addChild(view);
+    return view;
+  }
+
+  private pauseGame() {
+    this.isGamePaused = !this.isGamePaused;
+    const buttonText = this.pauseBtn.getChildAt(0) as Text;
+    if (this.isGamePaused) {
+      this.app.ticker.stop();
+      buttonText.text = 'Start';
+    } else {
+      this.app.ticker.start();
+      buttonText.text = 'Pause';
+    }
+    this.app.renderer.render(this.app.stage);
+  }
+
+  private createButtons(
+    x: number,
+    y: number,
+    btnText: string,
+    callBack: () => void
+  ) {
+    const view = new Graphics();
+    view.rect(0, 0, PIXEL_SIZE * 4, PIXEL_SIZE * 2);
+    view.fill(PIXEL_COLOR);
+    view.rect(4, 4, PIXEL_SIZE * 3.5, PIXEL_SIZE * 1.5);
+    view.fill(BG_PIXEL_COLOR);
+    view.x = x;
+    view.y = y;
+    const style = {fill: COLOR, fontSize: 18};
+    const text = new Text({text: btnText, style});
+    text.x = 6;
+    text.y = 6;
+    view.addChild(text);
+    view.interactive = true;
+    view.cursor = 'pointer';
+    view.on('click', callBack);
+    this.app.stage.addChild(view);
+
     return view;
   }
 }
